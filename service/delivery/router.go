cefc61dc0dc12ff3a97764d5f99fbc065211bbb8
agengnugroho93 <agengnugroho93@gmail.com>
2020-07-03 23:00:05 +0700

    [add] add router for login logut

--- service/delivery/router.go
+++ service/delivery/router.go
@@ -12,6 +12,11 @@ var router = mux.NewRouter()
 
 // Router init
 func Router() {
+	router.HandleFunc("/", homeHandler)
+	router.HandleFunc("/internal", pageHandler)
+
+	router.HandleFunc("/login", loginHandler).Methods("POST")
+	router.HandleFunc("/logout", logoutHandler).Methods("POST")
 
 	http.Handle("/", router)
 	fmt.Println("server started at localhost:8000")
