9e4070753314a488d2628d85f06634f8a41b2183
agengnugroho93 <agengnugroho93@gmail.com>
2020-07-03 22:59:12 +0700

    [add] add models for soon

--- models/e-wallet.go
+++ models/e-wallet.go
@@ -1,1 +1,15 @@
 package models
+
+type (
+	// GetUserID delcare
+	GetUserID struct {
+		ID int `json:"id"`
+	}
+
+	// GetBalance declare
+	GetBalance struct {
+		ID      int `json:"id"`
+		UserID  int `json:"user_id"`
+		Balance int `json:"balance"`
+	}
+)
